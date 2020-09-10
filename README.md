30999ae4ef26336b3d44eadcc2b9563ff2b50409
Matthew Pollicito <mpollicito@yahoo.com>
2020-09-10 14:19:15 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # homework6
 Weather Dashboard
+
+This app will be using APIs to gather weather information
---
+++ script.js
@@ -0,0 +1,25 @@
+let displayWeather = $(".weather-temp");
+
+
+
+function searchWeather () {
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=New york&appid=121dbfe48a774b52280de2af5c07925e"
+    $.ajax({
+        type: "GET",
+        url: queryUrl
+    })
+    .then(function(response) {
+        console.log(response);
+        let temp = "Temperature: " + convertKelvin(response.main.temp);
+        let humidity = "Humidity: " + response.main.humidity;
+        let windSpeed = "Wind Speed: " + response.wind.speed;
+
+        displayWeather.append(temp + "<br>", humidity + "<br>", windSpeed);
+    })
+}
+
+searchWeather();
+
+let convertKelvin = (kelvin) => {
+    return parseInt(((((kelvin) - 273.15) * 1.8) + 32));
+}
