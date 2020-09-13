0ab3c22355c998ed4717c9512d1a99314dc63048
Matthew Pollicito <mpollicito@yahoo.com>
2020-09-13 19:36:32 -0400

    Third Commit

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-let displayWeather = $(".weather-temp");
-let displayHumid = $(".weather-humidity");
-let displayWind = $(".weather-wind");
+$(document).ready(function () {
+
+    let displayWeather = $(".weather-temp");
+
+    function searchWeather() {
+        let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=Hartford&appid=121dbfe48a774b52280de2af5c07925e"
+        $.ajax({
+            type: "GET",
+            url: queryUrl
+        })
+            .then(function (response) {
+                console.log(response);
+                let temp = "Temperature: " + convertKelvin(response.main.temp) + " °F";
+                let humidity = "Humidity: " + response.main.humidity;
+                let windSpeed = "Wind Speed: " + response.wind.speed + " mph";
+
+                displayWeather.append(temp + "<br>", humidity + "<br>", windSpeed + "<br>");
+            })
+
+        let uvindexUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=121dbfe48a774b52280de2af5c07925e&lat=41.77&lon=-72.77"
+        $.ajax({
+            type: "GET",
+            url: uvindexUrl
+        })
+            .then(function (response) {
+                console.log(response);
+                let uvIndex = "UV Index: " + response.value;
+
+                displayWeather.append(uvIndex);
+            })
+    }
+
+    searchWeather();
 
 
-function searchWeather () {
-    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=Hartford&appid=121dbfe48a774b52280de2af5c07925e"
-    $.ajax({
-        type: "GET",
-        url: queryUrl
-    })
-    .then(function(response) {
-        console.log(response);
-        let temp = "Temperature: " + convertKelvin(response.main.temp);
-        let humidity = "Humidity: " + response.main.humidity;
-        let windSpeed = "Wind Speed: " + response.wind.speed;
-        
-        displayWeather.append(temp + "<br>", humidity + "<br>", windSpeed + "<br>");
-    })
+    let convertKelvin = (kelvin) => {
+        return parseInt(((((kelvin) - 273.15) * 1.8) + 32));
+    }
 
-    let uvindexUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=121dbfe48a774b52280de2af5c07925e&lat=41.77&lon=-72.77"
-    $.ajax({
-        type: "GET",
-        url: uvindexUrl
-    })
-    .then(function(response) {
-        console.log(response);
-        let uvIndex = "UV Index: " + response.value;
-
-        displayWeather.append(uvIndex);
-    })
-}
-
-searchWeather();
-
-let convertKelvin = (kelvin) => {
-    return parseInt(((((kelvin) - 273.15) * 1.8) + 32));
-}
+});
